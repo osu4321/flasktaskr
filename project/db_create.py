21e25a0c94018c8f31068c9391e17240a8129808
Christopher Ellis <cellis@ballethub.com>
2017-06-20 08:49:42 -0400

    Finished adding SQLAlchemy

--- project/db_create.py
+++ project/db_create.py
@@ -1,21 +1,15 @@
 # create database
 
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as conn:
+# Create db and table
+db.create_all()
 
-	# cursor
-	c = conn.cursor()
+#	insert	data
+db.session.add(Task("Finish	this tutorial",	date(2016, 9, 22), 10, 1))
+db.session.add(Task("Finish	Real Python", date(2016, 10, 3), 10, 1))
 
-	# Create table
-	c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-		status INTEGER NOT NULL)""")
-
-	# insert dummy data into table
-
-	c.execute('INSERT	INTO tasks	(name,	due_date,	priority,	status)'
-			'VALUES("Finish	this	tutorial",	"03/25/2015",	10,	1)')
-	c.execute('INSERT	INTO	tasks	(name,	due_date,	priority,	status)'
-			'VALUES("Finish	Real	Python	Course	2",	"03/25/2015",	10,	1)')
+#	commit	the	changes
+db.session.commit()
